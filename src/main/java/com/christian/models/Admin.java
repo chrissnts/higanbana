4de1242dc969cf997a7c3ba9f9457ce0c1ecb4da
chrissnts <christiandossantos125@gmail.com>
2025-10-03 12:39:18 -0300

    first commit

---
+++ src/main/java/com/christian/models/Admin.java
@@ -0,0 +1,7 @@
+package com.christian.models;
+
+public class Admin extends User {
+    public Admin(Long id, String name, String email, String password){
+        super(id, name, email, password);
+    }
+}
